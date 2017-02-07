fc6c4e8e9b9df340783718dd6c802a284741d2c7
Sam Ryan <sam.ryan.uofl@gmail.com>
2017-02-07 18:33:26 -0500

    Stopping for the day

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <iostream>
+#include <string>
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
@@ -10,22 +12,28 @@ int main(int argc, char* args[])
     SDL_Window* window = NULL;
     SDL_Surface* screenSurface = NULL;
 
-    if(SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
-        std::cout << "SDL Error";
+    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cout << "SDL Init Error";
         return -1;
     }
 
     window = SDL_CreateWindow("Civil War Strategy", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if(NULL == window)
-    {
-        std::cout << "SDL Error";
+    if(NULL == window) {
+        std::cout << "SDL Create Window Error";
     }
 
     screenSurface = SDL_GetWindowSurface(window);
     SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xff, 0xff, 0xff));
     SDL_UpdateWindowSurface(window);
+    SDL_Delay(1000);
+    SDL_Surface* mapSurface = IMG_Load("../res/EuropeMap.png");
+    if(NULL == mapSurface) {
+        std::cout << "SDL Load BMP Error: " << std::string(SDL_GetError()) << std::endl;
+    }
+    SDL_BlitSurface(mapSurface, NULL, screenSurface, NULL);
+    SDL_UpdateWindowSurface(window);
     SDL_Delay(2000);
+
 
     // Cleanup
     SDL_DestroyWindow(window);
